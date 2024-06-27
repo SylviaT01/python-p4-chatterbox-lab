d200d3441a03c13565a0804abb2327f12d735b1e
Sylvia Chebet <sylviachebet03@gmail.com>
2024-06-27 22:38:14 +0300

    create an API with Flask for a React frontend application.

--- server/app.py
+++ server/app.py
@@ -14,13 +14,45 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/')
+def home():
+    return '<h1>Messages</h1>'
 
-@app.route('/messages/<int:id>')
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+    # messages = [message.to_dict() for message in Message.query.order_by(Message.created_at.asc()).all()]
+    # return make_response(messages, 200)
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.order_by(Message.created_at.asc()).all()]
+        return make_response(messages, 200)
+    elif request.method == 'POST':
+        new_message = Message(
+            body = request.form.get("body"),
+            username = request.form.get("username")
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(), 201)
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+        message = Message.query.filter_by(id=id).first()
+        if message == None:
+            return make_response({"message": "Message not found"}, 404)
+        else:
+            if request.method == 'GET':
+                return make_response(message.to_dict(), 200)
+            elif request.method == 'PATCH':
+                for attr in request.form:
+                    setattr(message, attr, request.form[attr])
+                    db.session.add(message)
+                    db.session.commit()
+                    return make_response(message.to_dict(), 200)
+                
+            elif request.method == 'DELETE':
+                db.session.delete(message)
+                db.session.commit()
+                return make_response({'message': 'record successfully deleted'}, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
